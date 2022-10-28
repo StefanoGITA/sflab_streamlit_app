256d4f7791c0dca915c624504e670653e5d93cd9
sgiostra <sgiostra@bitbang.com>
2022-10-28 12:27:12 +0200

    fruityvice data to streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas as pd
 import requests
 
+
 streamlit.title(" My parents new healthy Diner")
 
 streamlit.header("Breakfast Favorites")
@@ -9,13 +10,13 @@ streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
 streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard-Boiled Free-Range Eggs")
 streamlit.text("🥑🍞 Avocado Toast")
-   
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Let's put a pick list here so they can pick the fruit they want to include 
+# Let's put a pick list here so they can pick the fruit they want to include
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Banana', 'Pineapple'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
@@ -24,4 +25,9 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+# streamlit.text(fruityvice_response.json())
+
+# Convert a json to dataframe
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+# Put the dataframe in streamlit
+streamlit.dataframe(fruityvice_normalized)
